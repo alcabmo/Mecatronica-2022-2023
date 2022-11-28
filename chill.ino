0156651ad6e87f40ecabf54e6dea8e858b70350b
alcabmo <78978241+alcabmo@users.noreply.github.com>
2022-11-28 10:26:26 +0100

    para hacerlo en casa

---
+++ chill.ino
@@ -0,0 +1,91 @@
+/*
+   -- New project --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 3.1.8 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.11.1 or later version;
+     - for iOS 1.9.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__SOFTSERIAL
+#include <SoftwareSerial.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL_RX 10
+#define REMOTEXY_SERIAL_TX 11
+#define REMOTEXY_SERIAL_SPEED 9600
+
+#define LED_PIN1 7
+#define LED_PIN2 8
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 37 bytes
+  { 255,2,0,11,0,30,0,16,24,0,1,0,22,24,12,12,2,31,0,1,
+  0,64,24,12,12,2,31,0,67,4,38,8,20,5,2,26,11 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t button_1; // =1 if button pressed, else =0 
+  uint8_t button_2; // =1 if button pressed, else =0 
+
+    // output variables
+  char test[11];  // string UTF8 end zero 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+
+void setup() 
+{
+  RemoteXY_Init (); 
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  
+  // TODO you setup code
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  if(button_1){
+    digitalWrite(LED_PIN1, HIGH);
+  }
+
+  if(button_2){
+    digitalWrite(LED_PIN2, HIGH);
+  }
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay() 
+
+
+}
